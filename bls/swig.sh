3dcd77eb9697c9a900fc5b93eab9829ff7871b62
HashEngineering <hashengineeringsolutions@gmail.com>
2022-08-27 23:27:20 -0700

    chore: remove replacements in swig.sh

--- bls/swig.sh
+++ bls/swig.sh
@@ -3,5 +3,3 @@
 rm -rf src/main/java/org/dashj/bls/v1
 mkdir src/main/java/org/dashj/bls/v1
 swig -v -c++ -java -package org.dashj.bls.v1 -outdir src/main/java/org/dashj/bls/v1 -o src/main/cpp/bls-signatures-v1.cpp src/main/swig/bls-signatures.i
-sed -ie "s/ Bytes/ bls::Bytes/g" src/main/cpp/bls-signatures-v1.cpp
-sed -ie "s/(Bytes/(bls::Bytes/g" src/main/cpp/bls-signatures-v1.cpp
