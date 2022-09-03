079e3d8f62d8c8d2850ca7eadbc3ab5e561a6f9f
HashEngineering <hashengineeringsolutions@gmail.com>
2022-09-02 17:43:04 -0700

    chore: simplify swig generator script

--- bls/swig.sh
+++ bls/swig.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
 #  -debug-symbols
-rm -rf src/main/java/org/dashj/bls/v1
-mkdir src/main/java/org/dashj/bls/v1
-swig -v -c++ -java -package org.dashj.bls.v1 -outdir src/main/java/org/dashj/bls/v1 -o src/main/cpp/bls-signatures-v1.cpp src/main/swig/bls-signatures.i
+swig -v -c++ -java -package org.dashj.bls -outdir src/main/java/org/dashj/bls -o src/main/cpp/bls-signatures-v1.cpp src/main/swig/bls-signatures.i
